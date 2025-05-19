dc843e8f98363d2b185fc34c9d45f9dad169d1cc
Evelynesumai <esumaievelyn@gmail.com>
2025-05-19 09:08:38 +0100

    Initial commit

--- microservices-python-app-main/Eks-terraform/backend.tf
+++ microservices-python-app-main/Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mudit-mathur0097" # Replace with your actual unique S3 bucket name
+    bucket = "microbucketcba" # Replace with your actual unique S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1" # Ensure this matches your AWS S3 bucket's region
+    region = "eu-west-2" # Ensure this matches your AWS S3 bucket's region
   }
 }
